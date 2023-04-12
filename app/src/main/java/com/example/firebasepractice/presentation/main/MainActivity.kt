bfc188b2f983da526e1b08c2f4ccb104132ed54f
unam <dnska6657@naver.com>
2023-04-12 16:59:46 +0900

    [chore] 주석 정리

--- app/src/main/java/com/example/firebasepractice/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/firebasepractice/presentation/main/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.firebasepractice.presentation.main
 
-import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
@@ -10,13 +9,11 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.selection.SelectionTracker
 import com.example.firebasepractice.R
 import com.example.firebasepractice.data.MainData
 import com.example.firebasepractice.databinding.ActivityMainBinding
 import com.example.firebasepractice.presentation.detail.DetailActivity
 import com.example.firebasepractice.presentation.main.adapter.MainAdapter
-import com.example.firebasepractice.presentation.main.adapter.setSelectionTracker
 import com.example.util.callback.OnItemClick
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -30,12 +27,7 @@ class MainActivity : AppCompatActivity(), OnItemClick {
 
     private val viewModel: MainViewModel by viewModels()
 
-    lateinit var selectionTracker: SelectionTracker<Long>
-
-
     val db = Firebase.firestore // Cloud Firestore 인스턴스 초기화
-
-    val mainDataList = arrayListOf<MainData>()
 
     lateinit var inputData: HashMap<String, String>
 
@@ -53,28 +45,21 @@ class MainActivity : AppCompatActivity(), OnItemClick {
         etController()
         addData()
         updateData()
-//        deleteData()
-        addTrackerObserver() //selection
 
     }
 
-    //https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074__480.jpg 강남 보호소
-
-    //http://img.seoul.co.kr//img/upload/2021/04/30/SSI_20210430195948.jpg 제주도 보호소 5
     private fun initAdapter() {
         mainAdapter = MainAdapter(this)
         binding.recyclerViewMain.adapter = mainAdapter
     }
 
     private fun etController() {
-
         binding.etInputImage.addTextChangedListener {
             if (!binding.etInputImage.text.isNullOrEmpty()) {
                 viewModel.inputImageUrl.value = binding.etInputImage.text.toString()
                 Log.d(TAG, "inputImageUrl 값? : ${viewModel.inputImageUrl.value}")
             }
         }
-
         binding.etInputName.addTextChangedListener {
             if (!binding.etInputName.text.isNullOrEmpty()) {
                 viewModel.inputName.value = binding.etInputName.text.toString()
@@ -83,6 +68,7 @@ class MainActivity : AppCompatActivity(), OnItemClick {
         }
     }
 
+    //firestore의 data를 갱신하는 함수
     private fun updateData() {
         binding.btnUpdateData.setOnClickListener {
             val washingtonRef = db.collection("data").document("5")
@@ -95,13 +81,14 @@ class MainActivity : AppCompatActivity(), OnItemClick {
         }
     }
 
+    //firestore에 data를 추가하는 함수
     private fun addData() {
         binding.btnInputData.setOnClickListener {
             inputData = hashMapOf(
                 "imageUrl" to "${viewModel.inputImageUrl.value}",
                 "name" to "${viewModel.inputName.value}"
             )
-            //add 메서드는 문서ID 값을 자동으로 생성하기 때문에 내가 설정한 문서ID 값으로 data를 추가하고 싶으면 set 메서드를 사용해야 함.
+            //지금 사용하고 싶은 add 메서드는 문서ID 값을 자동으로 생성하기 때문에 만약 내가 설정한 문서ID 값으로 data를 추가하고 싶으면 set 메서드를 사용해야 함.
             db.collection("data").add(inputData)
                 .addOnSuccessListener { documentReference ->
                     Toast.makeText(this@MainActivity, "data 추가 성공", Toast.LENGTH_SHORT).show()
@@ -114,27 +101,33 @@ class MainActivity : AppCompatActivity(), OnItemClick {
         }
     }
 
-
+    //firestore에서 data를 받아오는 함수
     private fun getFirebaseData() {
         db.collection("data")
             .addSnapshotListener { snapshot, e ->
+                //get은 일회성 data 수신이지만 addSnapshotListener는 firestore의 data에 변경 사항이 있을 경우 실시간으로 받아옴
                 if (e != null) {
                     Log.w(TAG, "Listen failed.", e)
                     return@addSnapshotListener
                 }
-
-                val mainDataList = mutableListOf<MainData>()
-                for (document in snapshot!!) {//!! 안 붙이면 에러 떠서
-                    val imageUrl = document.getString("imageUrl")
-                    val placeName = document.getString("name")
-                    val mainData = MainData(imageUrl = imageUrl.toString(), placeName = placeName.toString())
-                    mainDataList.add(mainData)
+                if (snapshot != null) { //snapshot은 변경 사항이 담긴 객체
+                    val mainDataList = mutableListOf<MainData>()
+                    for (document in snapshot) {
+                        val imageUrl = document.getString("imageUrl")
+                        val placeName = document.getString("name")
+                        val mainData = MainData(
+                            imageUrl = imageUrl.toString(),
+                            placeName = placeName.toString()
+                        )
+                        mainDataList.add(mainData)
+                    }
+                    viewModel.mainDataList.value = mainDataList
+                    Log.d(TAG, "뷰모델 mainDataList 값 : ${viewModel.mainDataList.value}")
                 }
-                viewModel.mainDataList.value = mainDataList
-                Log.d(TAG, "뷰모델 mainDataList 값 : ${viewModel.mainDataList.value}")
+
+
             }
     }
-
 
 
     private fun addObserver() {
@@ -142,38 +135,6 @@ class MainActivity : AppCompatActivity(), OnItemClick {
             mainAdapter.submitList(viewModel.mainDataList.value)
         }
     }
-
-    //SelectionTracker.kt로 빼준 함수 활용
-    private fun addTrackerObserver() {
-        selectionTracker =
-            setSelectionTracker("StorageMyDrawSelectionTracker", binding.recyclerViewMain)
-        selectionTracker.addObserver((object : SelectionTracker.SelectionObserver<Long>() {
-
-
-            override fun onSelectionChanged() {
-                super.onSelectionChanged()
-                val items = selectionTracker.selection.size()
-                Log.d(ContentValues.TAG, "items 사이즈?: $items")
-                if (items == 0) {
-//                    binding.btnDelete.setBackgroundResource(R.drawable.radius_10_g3_button)
-                    binding.enabled = false
-                } else if (items >= 1) {
-//                    binding.btnDelete.setBackgroundResource(R.drawable.radius_10_m1_button)
-                    binding.enabled = true
-                } // 선택된 아이템이 1개 이상일 경우 button 활성화 (floating button하고 그냥 버튼하고 기본 지원 옵션이 좀 다른 듯함.)
-
-            }
-        }))
-        mainAdapter.setSelectionTracker(selectionTracker) //어댑터 생성 후 할당해줘야 한다는 순서지킴
-    }
-
-
-//    private fun deleteData() {
-//        binding.btnDelete.setOnClickListener {
-//            mainAdapter.removeItem(selectionTracker.selection)
-//            selectionTracker.clearSelection()
-//        }
-//    }
 
 
     override fun selectItem(id: MainData) {
